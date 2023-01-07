e4a100f98c488c5d69d0e0242955d4f419cff33e
Dongnyeok Shin <dn.shin98@gmail.com>
2023-01-07 21:17:37 +0900

    [add]  CollectionView to HomeVC

---
+++ MyMemory/View/HomeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCollectionViewCell.swift
+//  MyMemory
+//
+//  Created by NYEOK on 2023/01/07.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- MyMemory/View/HomeViewController.swift
+++ MyMemory/View/HomeViewController.swift
@@ -7,20 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+class HomeViewController: UIViewController {
     // MARK: - PROPERTIES
     
+    @IBOutlet weak var HomeCollectionView: UICollectionView!
     
 
     // MARK: - LIFECYCLE
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    
     // MARK: - HELPER
-
 }
-
