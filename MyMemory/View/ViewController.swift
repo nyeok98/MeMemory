562828af7ec79c458c6a363ac6f55ad07eb5787a
Dongnyeok Shin <dn.shin98@gmail.com>
2023-01-07 21:06:16 +0900

    [set] base

---
+++ MyMemory/View/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by NYEOK on 2023/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - PROPERTIES
+    
+    
+
+    // MARK: - LIFECYCLE
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    // MARK: - HELPER
+
+}
+
