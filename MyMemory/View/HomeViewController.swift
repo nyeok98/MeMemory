8d9211a44fda3d770465e694c974ee0e6b3d5109
Dongnyeok Shin <dn.shin98@gmail.com>
2023-01-10 17:21:00 +0900

    [add] Cells

--- MyMemory/View/HomeViewController.swift
+++ MyMemory/View/HomeViewController.swift
@@ -9,16 +9,29 @@ import UIKit
 
 class HomeViewController: UIViewController {
     // MARK: - PROPERTIES
-    
-    @IBOutlet weak var HomeCollectionView: UICollectionView!
-    
+
+    @IBOutlet var homeCollectionView: UICollectionView!
 
     // MARK: - LIFECYCLE
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        homeCollectionView.delegate = self
         // Do any additional setup after loading the view.
     }
 
     // MARK: - HELPER
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        2
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = homeCollectionView.dequeueReusableCell(withReuseIdentifier: "HomeCollectionViewCell", for: indexPath) as? HomeCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
 }
