ae768f6785187906bbedd40d03c02c699a13ec80
Dongnyeok Shin <dn.shin98@gmail.com>
2023-01-26 19:09:32 +0900

    [add] Codable for JSON Serialization

--- MyMemory/View/HomeViewController.swift
+++ MyMemory/View/HomeViewController.swift
@@ -6,43 +6,70 @@
 //
 
 import Alamofire
+import FirebaseDatabase
 import SwiftyJSON
 import UIKit
-import FirebaseDatabase
 
 class HomeViewController: UIViewController {
     // MARK: - PROPERTIES
-    var ref: DatabaseReference!
 
-    @IBOutlet var homeCollectionView: UICollectionView!
-    var dayArray: [Day] = []
+    let db = Database.database().reference().child("Day")
+
+    @IBOutlet var dayCollectionView: UICollectionView!
+    var dayList: [Day] = []
 
     // MARK: - LIFECYCLE
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        homeCollectionView.delegate = self
-        homeCollectionView.dataSource = self
-        let dayNib = UINib(nibName: "DayCollectionViewCell", bundle: nil)
-        homeCollectionView.register(dayNib, forCellWithReuseIdentifier: "DayCollectionViewCell")
-        
-        self.ref = Database.database().reference()
-        let itemRef = self.ref.child("list")
-        itemRef.setValue(1)
+        dayCollectionView.delegate = self
+        dayCollectionView.dataSource = self
+        registerNib()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        fetchDayData()
     }
 
     // MARK: - HELPER
+
+    func fetchDayData() {
+        db.observeSingleEvent(of: .value) { snapshot in
+            guard let snapData = snapshot.value as? [String: Any] else { return }
+            let data = try! JSONSerialization.data(withJSONObject: Array(snapData.values), options: [])
+            do {
+                let decoder = JSONDecoder()
+                let dayList = try decoder.decode([Day].self, from: data)
+                self.dayList = dayList
+                self.dayCollectionView.reloadData()
+            } catch {
+                print("\(error.localizedDescription)")
+            }
+        }
+    }
+
+    private func registerNib() {
+        let dayNib = UINib(nibName: "DayCollectionViewCell", bundle: nil)
+        dayCollectionView.register(dayNib, forCellWithReuseIdentifier: "DayCollectionViewCell")
+    }
 }
 
-extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        2
+        return dayList.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = homeCollectionView.dequeueReusableCell(withReuseIdentifier: "DayCollectionViewCell", for: indexPath) as? DayCollectionViewCell else {
+        guard let cell = dayCollectionView.dequeueReusableCell(withReuseIdentifier: "DayCollectionViewCell", for: indexPath) as? DayCollectionViewCell else {
             return UICollectionViewCell()
         }
+        cell.title.text = dayList[indexPath.row].title
+        cell.body.text = dayList[indexPath.row].body
         return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 300, height: 100)
     }
 }
