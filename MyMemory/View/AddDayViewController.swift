a2acb4c565deb953f367e12b0ae2de9087e847a1
nyeok98 <dn.shin98@gmail.com>
2023-01-30 20:11:24 +0900

    [set] AddDayVC

--- MyMemory/View/AddDayViewController.swift
+++ MyMemory/View/AddDayViewController.swift
@@ -8,9 +8,19 @@
 import UIKit
 
 class AddDayViewController: UIViewController {
+    // MARK: - PROPERTIES
     
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    
+    // MARK: - LIFECYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    
+    // MARK: - HELPERS
+    @IBAction func finishButtonTapped(_ sender: Any) {
+        dismiss(animated: true)
+    }
 }
